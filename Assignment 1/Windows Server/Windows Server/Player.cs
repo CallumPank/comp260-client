12d9f87be1341d5e8364f278c1368af04ac45b82
Callum <callpank97@gmail.com>
2018-07-24 12:12:35 +0100

    Things

---
+++ Assignment 1/Windows Server/Windows Server/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public class Player
+    {
+        public Dungeon dungeonRef;
+        public Room currentRoom;
+        public String playerName;
+
+        public void Init()
+        {
+            currentRoom = dungeonRef.roomMap["Room 1"]; ;
+        }
+    }
+}
